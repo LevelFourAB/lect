6033ee2937753712bfbb8cf5fffd505ba075b605
Andreas Holstenson <a@holstenson.se>
2017-07-14 09:09:27 +0200

    Adding OffsetTrackingReader

---
+++ lect-api/src/main/java/se/l4/lect/text/OffsetTrackingReader.java
@@ -0,0 +1,127 @@
+package se.l4.lect.text;
+
+import java.io.FilterReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Arrays;
+
+import se.l4.lect.TextLocation;
+
+/**
+ * {@link Reader} that tracks offsets and allows mapping between offset and line and column.
+ *
+ * @author Andreas Holstenson
+ *
+ */
+public class OffsetTrackingReader
+	extends FilterReader
+{
+	private int offset;
+	private int line;
+	private int[] lineStartOffsets;
+	private boolean lastWasCarriageReturn;
+
+	public OffsetTrackingReader(Reader in)
+	{
+		super(in);
+
+		line = 0;
+		lineStartOffsets = new int[64];
+	}
+
+	private void ensureCapacity(int c)
+	{
+		if(c < lineStartOffsets.length) return;
+
+		int length = lineStartOffsets.length;
+		int newSize = length + (length >> 1);
+
+		lineStartOffsets = Arrays.copyOf(lineStartOffsets, newSize);
+	}
+
+	@Override
+	public int read()
+		throws IOException
+	{
+		int result = super.read();
+		track(result);
+		return result;
+	}
+
+	@Override
+	public int read(char[] cbuf, int off, int len)
+		throws IOException
+	{
+		int result = super.read(cbuf, off, len);
+		track(cbuf, off, result);
+		return result;
+	}
+
+	private void track(int c)
+	{
+		if(c == -1) return;
+
+		if(c == '\r' || c == '\n' || c == '\u2028' || c == '\u2029' || c == '\u0085')
+		{
+			if(c == '\n' && lastWasCarriageReturn)
+			{
+				// \n following \r is still the same break, update the previous index
+				lineStartOffsets[line] = offset + 1;
+			}
+			else
+			{
+				if(c == '\r')
+				{
+					lastWasCarriageReturn = true;
+				}
+
+				line++;
+				ensureCapacity(line);
+				lineStartOffsets[line] = offset + 1;
+			}
+		}
+
+		offset++;
+	}
+
+	private void track(char[] cbuf, int off, int len)
+	{
+		for(int i=off, n=off+len; i<n; i++)
+		{
+			track(cbuf[i]);
+		}
+	}
+
+	/**
+	 * Resolve the {@link TextLocation} of a character offset.
+	 *
+	 * @param offset
+	 * @return
+	 */
+	public TextLocation offsetToLocation(int offset)
+	{
+		TextLocation loc = new TextLocation(0, 0);
+		offsetToLocation(offset, loc);
+		return loc;
+	}
+
+	/**
+	 * Resolve the {@link TextLocation} of a character offset and store it in the given target.
+	 *
+	 * @param offset
+	 * @param target
+	 */
+	public void offsetToLocation(int offset, TextLocation target)
+	{
+		int line = Arrays.binarySearch(lineStartOffsets, 0, this.line + 1, offset);
+		int column = 0;
+		if(line < 0)
+		{
+			// The offset was not exactly the start of a line, so figure out the nearest line and then the column
+			line = -(line + 1) - 1;
+			column = offset - lineStartOffsets[line];
+		}
+
+		target.moveTo(line, column);
+	}
+}
---
+++ lect-api/src/test/java/se/l4/lect/text/OffsetTrackingReaderTest.java
@@ -0,0 +1,122 @@
+package se.l4.lect.text;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+import org.junit.Test;
+
+import se.l4.lect.TextLocation;
+
+public class OffsetTrackingReaderTest
+{
+	private static OffsetTrackingReader read(String str)
+	{
+		try
+		{
+			OffsetTrackingReader r = new OffsetTrackingReader(new StringReader(str));
+			char[] buf =  new char[1024];
+			while(r.read(buf) != -1) {};
+			return r;
+		}
+		catch(IOException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Test
+	public void testSingleLineStart()
+	{
+		OffsetTrackingReader reader = read("Cookies are tasty");
+		TextLocation loc = reader.offsetToLocation(0);
+
+		assertThat(loc.getLine(), is(0));
+		assertThat(loc.getColumn(), is(0));
+	}
+
+	@Test
+	public void testSingleLineMiddle()
+	{
+		OffsetTrackingReader reader = read("Cookies are tasty");
+		TextLocation loc = reader.offsetToLocation(5);
+
+		assertThat(loc.getLine(), is(0));
+		assertThat(loc.getColumn(), is(5));
+	}
+
+	@Test
+	public void testTwoLinesMiddleOfFirstLine()
+	{
+		OffsetTrackingReader reader = read("Cookies\nare tasty");
+		TextLocation loc = reader.offsetToLocation(5);
+
+		assertThat(loc.getLine(), is(0));
+		assertThat(loc.getColumn(), is(5));
+	}
+
+	@Test
+	public void testTwoLinesEndOfFirstLine()
+	{
+		OffsetTrackingReader reader = read("Cookies\nare tasty");
+		TextLocation loc = reader.offsetToLocation(7);
+
+		assertThat(loc.getLine(), is(0));
+		assertThat(loc.getColumn(), is(7));
+	}
+
+	@Test
+	public void testTwoLinesStartAtSecondLine()
+	{
+		OffsetTrackingReader reader = read("Cookies\nare tasty");
+		TextLocation loc = reader.offsetToLocation(8);
+
+		assertThat(loc.getLine(), is(1));
+		assertThat(loc.getColumn(), is(0));
+	}
+
+	@Test
+	public void testTwoLinesMiddleInSecondLine()
+	{
+		OffsetTrackingReader reader = read("Cookies\nare tasty");
+		TextLocation loc = reader.offsetToLocation(12);
+
+		assertThat(loc.getLine(), is(1));
+		assertThat(loc.getColumn(), is(4));
+	}
+
+	@Test
+	public void testTwoLinesOutsideRange()
+	{
+		OffsetTrackingReader reader = read("Cookies\nare tasty");
+		TextLocation loc = reader.offsetToLocation(30);
+
+		assertThat(loc.getLine(), is(1));
+		assertThat(loc.getColumn(), is(22));
+	}
+
+	@Test
+	public void testCarriageReturn()
+	{
+		OffsetTrackingReader reader = read("Cookies\r\nare tasty");
+		TextLocation loc = reader.offsetToLocation(9);
+
+		assertThat(loc.getLine(), is(1));
+		assertThat(loc.getColumn(), is(0));
+	}
+
+	@Test
+	public void testMultipleNewLines()
+	{
+		OffsetTrackingReader reader = read("Cookies\n\nare tasty");
+		TextLocation loc = reader.offsetToLocation(8);
+		assertThat(loc.getLine(), is(1));
+		assertThat(loc.getColumn(), is(0));
+
+		loc = reader.offsetToLocation(9);
+		assertThat(loc.getLine(), is(2));
+		assertThat(loc.getColumn(), is(0));
+	}
+}
