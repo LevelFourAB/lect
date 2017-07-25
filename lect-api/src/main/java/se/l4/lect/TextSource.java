97531282144b45c55dd8b819df0ce9de737a06a8
Andreas Holstenson <a@holstenson.se>
2017-07-25 09:56:34 +0200

    TextSource.process to quickly create a PipelineRunner

--- lect-api/src/main/java/se/l4/lect/TextSource.java
+++ lect-api/src/main/java/se/l4/lect/TextSource.java
@@ -3,6 +3,7 @@ package se.l4.lect;
 import java.io.IOException;
 
 import se.l4.lect.internal.EmptyTextSource;
+
 /**
  * Abstraction over sources of text, used to support different formats such as plain text and HTML.
  *
@@ -19,6 +20,16 @@ public interface TextSource
 	 */
 	void parse(TextSourceEncounter encounter)
 		throws IOException;
+
+	/**
+	 * Start building a pipeline to process contents in this source.
+	 *
+	 * @return
+	 */
+	default PipelineRunner<?> process()
+	{
+		return Pipeline.over(this);
+	}
 
 	/**
 	 * Get a {@link TextSource} that is empty.
